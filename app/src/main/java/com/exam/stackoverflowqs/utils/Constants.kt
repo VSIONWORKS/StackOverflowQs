644aaaaa90b4cf1d68cb62093c31c8b6645e0ade
Kenneth Nueva <vsionworks@gmail.com>
2023-03-12 00:57:00 +0800

    Added new value to Constants

--- app/src/main/java/com/exam/stackoverflowqs/utils/Constants.kt
+++ app/src/main/java/com/exam/stackoverflowqs/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.exam.stackoverflowqs.utils
 object Constants {
 
     const val BASE_URL = "https://api.stackexchange.com/"
-
     const val THROWABLE = "Throwable"
+    const val LAST_DAYS_NO = 30
 }
