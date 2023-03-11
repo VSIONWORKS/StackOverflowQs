f59631921dca42b5393fe07cf297d60d89ebc274
Kenneth Nueva <vsionworks@gmail.com>
2023-03-12 00:53:07 +0800

    Set values for QuestionItem new views

--- app/src/main/java/com/exam/stackoverflowqs/ui/item/QuestionItem.kt
+++ app/src/main/java/com/exam/stackoverflowqs/ui/item/QuestionItem.kt
@@ -4,14 +4,23 @@ import android.view.View
 import com.exam.stackoverflowqs.R
 import com.exam.stackoverflowqs.data.model.Item
 import com.exam.stackoverflowqs.databinding.LayoutQuestionItemBinding
+import com.exam.stackoverflowqs.utils.load
+import com.exam.stackoverflowqs.utils.toFormmattedDate
 import com.xwray.groupie.viewbinding.BindableItem
 
 class QuestionItem(private val item: Item, private val onClick: (String) -> Unit) : BindableItem<LayoutQuestionItemBinding>() {
     override fun bind(viewBinding: LayoutQuestionItemBinding, position: Int) {
         viewBinding.apply {
-            tvTitle.text = item.title
-            tvAnswerLabel.text = "Answers : ${item.answerCount}"
-            root.setOnClickListener { onClick.invoke(item.link) }
+            with(item) {
+                civProfile.load(owner.profileImage)
+                tvName.text = owner.displayName
+                tvTitle.text = title
+                tvDate.text = creationDate.toFormmattedDate()
+                tvScoreValue.text = score.toString()
+                tvAnswerValue.text = answerCount.toString()
+                tvViewValue.text = viewCount.toString()
+                root.setOnClickListener { onClick.invoke(link) }
+            }
         }
     }
 
